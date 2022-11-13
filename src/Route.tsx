de4bb5317df97b4a16f76e3eeb59b3ac6e2a84b7
jemiks <jelo.kokhodze@gmail.com>
2022-11-13 18:19:14 +0400

    fix: update context

--- src/Route.tsx
+++ src/Route.tsx
@@ -25,53 +25,56 @@ import Contract from "pages/Contract";
 
 const Route = () => {
   return (
-	<>
-	  <Routes>
-		<Router element={<PrivateRoute/>}>
-		  
-		  <Router path="/" element={<Home/>}/>
-		  <Router path="channels" element={<Channels/>}/>
-		  <Router path="saved" element={<Saved/>}/>
-		  <Router path="wallets" element={<Wallets/>}/>
-		  <Router path="create" element={<Create/>}/>
-		  <Router path="/games" element={<Projects/>}/>
-		  <Router path={'change-password'} element={<ChangePassword/>}/>
-		  <Router path={'account'} element={<Account/>}/>
-		  
-		  <Router path="game/:id">
-			<Router index element={<EditProject/>}/>
-			<Router path={'collections'} element={<MainComponent value={'Collections'}/>}/>
-			<Router path={'contracts'} element={<Contract/>}/>
-		  </Router>
-		  
-		  <Router path="api-keys" element={<ApiKeys/>}/>
-		  <Router path="settings" element={<Settings/>}/>
-		  <Router path="logs" element={<Logs/>}/>
-		  <Router path="teams" element={<Teams/>}/>
-		  <Router path="doc" element={<Doc/>}/>
-		  <Router path="about" element={<About/>}/>
-		  <Router path="*" element={<MainComponent value={'page not found'}/>}/>
-		
-		</Router>
-		
-		<Router element={<PublicRoute/>}>
-		  <Router path="/login" element={<Login/>}/>
-		  <Router path="/register" element={<Register/>}/>
-		  <Router path="/forgot-password" element={<ForgotPassword/>}/>
-		  <Router path="/login/:id" element={<Login/>}/>
-		  <Router path="/reset-password/:id" element={<ResetPassword/>}/>
-		  <Router path="/authentication/:id" element={<TwoFAuthentication/>}/>
-		</Router>
-		
-		
-		{/* <Route path="/" element={<PrivateRoute />}>
+    <>
+      <Routes>
+        <Router element={<PrivateRoute />}>
+          <Router path="/" element={<Home />} />
+          <Router path="channels" element={<Channels />} />
+          <Router path="saved" element={<Saved />} />
+          <Router path="wallets" element={<Wallets />} />
+          <Router path="create" element={<Create />} />
+          <Router path="/games" element={<Projects />} />
+          <Router path={"change-password"} element={<ChangePassword />} />
+          <Router path={"account"} element={<Account />} />
+
+          <Router path="game/:id">
+            <Router index element={<EditProject />} />
+            <Router
+              path={"collections"}
+              element={<MainComponent value={"Collections"} />}
+            />
+            <Router path={"contracts"} element={<Contract />} />
+          </Router>
+
+          <Router path="api-keys" element={<ApiKeys />} />
+          <Router path="settings" element={<Settings />} />
+          <Router path="logs" element={<Logs />} />
+          <Router path="teams" element={<Teams />} />
+          <Router path="doc" element={<Doc />} />
+          <Router path="about" element={<About />} />
+          <Router
+            path="*"
+            element={<MainComponent value={"page not found"} />}
+          />
+        </Router>
+
+        <Router element={<PublicRoute />}>
+          <Router path="/login" element={<Login />} />
+          <Router path="/register" element={<Register />} />
+          <Router path="/forgot-password" element={<ForgotPassword />} />
+          <Router path="/login/:id" element={<Login />} />
+          <Router path="/reset-password/:id" element={<ResetPassword />} />
+          <Router path="/authentication/:id" element={<TwoFAuthentication />} />
+        </Router>
+
+        {/* <Route path="/" element={<PrivateRoute />}>
 		 <Route path="/home" element={<Administration />} />
 		 <Route path="/dashboard" element={<Administration />} />
 		 <Route path="/administration" element={<Administration />} />
 		 </Route>       */}
-	  </Routes>
-	  <DeleteConfirmationModal/>
-	</>
+      </Routes>
+      <DeleteConfirmationModal />
+    </>
   )
 }
 
--- src/contexts.ts
+++ src/contexts.ts
@@ -1,18 +1,54 @@
 import React from 'react'
 
+type Vehicle = {
+  id: string
+  make: string
+  model: string
+  img_source: string
+  power_train_type: string
+  trim_name: string
+  year: string
+  drive_transmission: string
+}
+
+type ComparisonContextValue = {
+  vehicles: Array<Vehicle>
+  clear: () => void
+  addVehicle: (vehicle: Vehicle) => void
+  removeVehicle: (vehicleId: string) => void
+  isCompared: (vehicleId: string) => boolean
+}
+
 type UserContextValue = {
-  user: any,
-  loading: boolean,
-  isAuthenticated: boolean,
+  user: any
+  loading: boolean
+  isAuthenticated: boolean
   account: any
 }
 
 type ModalContextValue = {
-  modals: object,
-  openModal: (params: { name: string, data?: any }) => void,
-  closeModal: (name: string) => void,
+  modals: object
+  openModal: (params: { name: string; data?: any }) => void
+  closeModal: (name: string) => void
 }
 
+type PageStateContextValue = {
+  pageStateValue: any
+  updatePageStateValue: (value: any) => void
+}
+
+export const PageStateContext = React.createContext<PageStateContextValue>({
+  pageStateValue: {},
+  updatePageStateValue: () => {},
+})
+
+export const ComparisonContext = React.createContext<ComparisonContextValue>({
+  vehicles: [],
+  addVehicle: () => {},
+  clear: () => {},
+  removeVehicle: () => {},
+  isCompared: () => false,
+})
 
 export const AuthContext = React.createContext<UserContextValue>({
   user: null,
@@ -27,4 +63,12 @@ export const ModalContext = React.createContext<ModalContextValue>({
   closeModal: (name) => {},
 })
 
+type ScenarioContextType = {
+  vehicle: any
+  calculations: any
+}
 
+export const ScenarioContext = React.createContext<ScenarioContextType>({
+  vehicle: null,
+  calculations: null,
+})
