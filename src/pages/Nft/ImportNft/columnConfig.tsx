74e86ad109fa93744036b4f011907fedf7306db1
changelia <changelia97@gmail.com>
2022-12-22 17:14:00 +0400

    fix: import nfts response

--- src/pages/Nft/ImportNft/columnConfig.tsx
+++ src/pages/Nft/ImportNft/columnConfig.tsx
@@ -8,11 +8,22 @@ export const columnConfig = ({ keys }: { keys: any }) => {
 }
 
 export const importedColumnConfig = (array: any) => {
-  const { __typename, id, ...args } = array[0] ?? {}
-  return [...Object.keys(args).map(item => ({ dataKey: item, name: item }))]
+  const { __typename, id, properties, custom_props, parent_id, asset_url, ...args } = array[0] ?? {}
+  return [
+    ...Object.keys(args).map(item => ({ dataKey: item, name: item })), 
+    { dataKey: (row: any) => row.properties.map((item: any) => item.name), name: 'properties' },
+    { dataKey: (row: any) => row.custom_props.map((item: any) => item[Object.keys(item)[0]]), name: 'custom_field' },
+    { dataKey: (row: any) => !row.asset_url ? <img width="50px" src={row.asset_url} /> : '', name: 'asset_url' },
+    // ...custom_props.map((i: any) => ({ dataKey: i[Object.keys(i)[0]], name: i[Object.keys(i)[0]] })),
+    // { dataKey: (row: any) => Object.keys(row.custom_props) },
+  ]
 }
 
 export const notImportedColumnConfig = (array: any) => {
-  const { __typename, ...args } = array[0] ?? {}
-  return [...Object.keys(args).map(item => ({ dataKey: item, name: item }))]
+  const { __typename, custom_props, asset_url, parent_id, ...args } = array[0] ?? {}
+  return [
+    ...Object.keys(args).map(item => ({ dataKey: item, name: item })),
+    { dataKey: (row: any) => row.custom_props.map((item: any) => item[Object.keys(item)[0]]), name: 'custom_field' },
+    { dataKey: (row: any) => !row.asset_url ? <img width="50px" src={row.asset_url} /> : '', name: 'asset_url' },
+  ]
 }
