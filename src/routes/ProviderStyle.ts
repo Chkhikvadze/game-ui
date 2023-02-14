c3e556bf4348721edf10d4d25c4642a42b277388
kokhodze <jelo.kokhodze@gmail.com>
2023-02-14 23:37:18 +0400

    fix: scroll

--- src/routes/ProviderStyle.ts
+++ src/routes/ProviderStyle.ts
@@ -25,8 +25,9 @@ const StyledMainSection = styled.div`
   background: rgba(255, 255, 255, 0.1);
   border-radius: 10px;
   overflow: auto;
-  height: 100%;
+  max-height: 100vh;
   padding: 20px;
+  overflow: auto;
   /* padding-right: 0; */
 `
 
