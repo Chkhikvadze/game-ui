913ebe3c6c1868ecd609ec62d56c7d49d0591ea1
Mirian Okradze <okradzemirian@gmail.com>
2023-03-27 18:32:11 -0500

    feat: replace sample solidity code

--- src/pages/Contract/ContractForm/CreateContractForm.tsx
+++ src/pages/Contract/ContractForm/CreateContractForm.tsx
@@ -60,27 +60,36 @@ const CreateContractForm = ({ closeModal }: CreateContractFormProps) => {
     third: false,
   })
 
-  const code = `// Imports
-  import mongoose, { Schema } from 'mongoose'
-  
-  // Collection name
-  export const collection = 'Product'|
-  
-  // Schema
-  const schema = new Schema({
-    name: {
-      type: String,
-      required: true
-    },
-  
-    description: {
-      type: String
-    }
-  }, {timestamps: true})
-  
-  // Model
-  export default mongoose.model(collection, schema, collection)
-  `
+  const code = `
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import '@openzeppelin/contracts/token/ERC1155/ERC1155.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+import './WeaponSupply.sol';
+
+contract Main is ERC1155, Ownable, WeaponSupply {
+  constructor() ERC1155('{{token_uri}}') {}
+
+  function setURI(string memory newuri) public onlyOwner {
+    _setURI(newuri);
+  }
+
+  function mint(address account, uint256 id, uint256 amount, bytes memory data)
+    public onlyOwner {
+    _mint(account, id, amount, data);
+  }
+
+  function mintBatch(
+    address to,
+    uint256[] memory ids,
+    uint256[] memory amounts,
+    bytes memory data
+  ) public onlyOwner {
+    _mintBatch(to, ids, amounts, data);
+  }
+}
+`
 
   const [showCode, setShowCode] = useState(false)
   console.log('tomorrowNightBlue', tomorrowNightBlue)
