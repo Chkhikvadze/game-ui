bfa4ba7b86497e9e65c309b2493b824dc0b47736
kokhodze <jelo.kokhodze@gmail.com>
2023-04-11 18:11:38 +0400

    fix: default images

--- src/pages/Collection/EditCollection/Appearance/Appearance.tsx
+++ src/pages/Collection/EditCollection/Appearance/Appearance.tsx
@@ -48,6 +48,17 @@ const Appearance = () => {
 
   const isLoading = uploadImageLoading || setDefaultMediaLoading
 
+  const default_images = [{ url: background }, { url: background2 }, { url: background3 }]
+
+  const merged_images =
+    collection_images?.length > 0
+      ? default_images.map((item, index) =>
+          collection_images[index] ? (item = collection_images[index]) : default_images[index],
+        )
+      : default_images
+
+  const media_array = collection_images?.length <= 3 ? merged_images : collection_images
+
   return (
     <StyledRoot>
       <StyledMediaWrapper>
@@ -74,40 +85,29 @@ const Appearance = () => {
         </StyledTextWrapper>
 
         <StyledCollectionScroll>
-          {collection_images?.length > 0 ? (
-            collection_images?.map((item: any) => {
-              const isMainMedia = item.is_main
-              return (
-                <>
-                  {isImage(item.url) && (
-                    <StyledImageWrapper key={item.id} isMain={isMainMedia}>
-                      <StyledImage src={item.url} alt='' />
+          {media_array?.map((item: any) => {
+            const isMainMedia = item.is_main
+            return (
+              <>
+                {isImage(item.url) && (
+                  <StyledImageWrapper key={item.id} isMain={isMainMedia}>
+                    <StyledImage src={item.url} alt='' />
+                    {item.id && (
                       <StyledHoverContainer onClick={() => onSetDefaultCollectionMedia(item.id)}>
                         <span>Set as main</span>
                       </StyledHoverContainer>
-                    </StyledImageWrapper>
-                  )}
-                  {isVideo(item.url) && (
-                    <StyledWrapper>
-                      <video src={item.url} width='100%' height='100%' controls></video>
-                    </StyledWrapper>
-                  )}
-                </>
-              )
-            })
-          ) : (
-            <>
-              <StyledImageWrapper>
-                <StyledImage src={background} alt='' />
-              </StyledImageWrapper>
-              <StyledImageWrapper>
-                <StyledImage src={background2} alt='' />
-              </StyledImageWrapper>
-              <StyledImageWrapper>
-                <StyledImage src={background3} alt='' />
-              </StyledImageWrapper>
-            </>
-          )}
+                    )}
+                  </StyledImageWrapper>
+                )}
+                {isVideo(item.url) && (
+                  <StyledWrapper>
+                    <video src={item.url} width='100%' height='100%' controls></video>
+                  </StyledWrapper>
+                )}
+              </>
+            )
+          })}
+
           {isLoading && (
             <StyledLoadingContainer className='loading'>Loading...</StyledLoadingContainer>
           )}
