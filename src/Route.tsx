bc73651962c7c3d472bfa552dfa140d435f27197
jemiks <jelo.kokhodze@gmail.com>
2023-05-01 19:20:53 +0400

    fix: routing

--- src/Route.tsx
+++ src/Route.tsx
@@ -62,10 +62,10 @@ import PlayerInfo from 'pages/Player/PlayerInfo'
 import PlayerAssets from 'pages/Player/PlayerAssets'
 import PlayerTransactions from 'pages/Player/PlayerTransactions'
 import SpotlightModal from 'modals/SpotlightModal'
-import Spotlight from 'components/Spotlight'
 import ContactInfoModal from 'modals/ContactInfoModal'
 import CreateGameModal from 'modals/CreateGameModal'
 import CreateCollectionModal from 'modals/CreateCollectionModal'
+import Spotlight from 'components/Spotlight/Spotlight'
 
 // import GameRoute from "oldComponents/atoms/routerProviders/GameRoute";
 // import ManageUsers from "pages/Admin/ManageUsers"
@@ -80,41 +80,16 @@ const Route = () => {
     <ThemeProvider theme={theme}>
       {user && (
         <>
-          {user?.role === 'admin' ? (
-            <Router element={<AdminRoute />}>
-              <Router path='/' element={<ManageUsers />} />
-              <Router path='/admin/users/create' element={<CreateUser />} />
-              <Router path='/admin/user/edit/:id' element={<EditUser />} />
-              <Router path='/admin/user/:id' element={<ViewUser />} />
-              <Router path='/admin/user/edit/update-role/:id' element={<UpdateRole />} />
-            </Router>
-          ) : (
-            <Router>
-              <Router element={<PrivateRoute />}>
-                <Router path='/' element={<Home />} />
-                <Router path='game' element={<Games />} />
-                <Router path='teams' element={<Teams />} />
-                <Router path='channels' element={<Channels />} />
-                <Router path='developers' element={<Navigate to={'api-keys'} />} />
-
-                {/* // disabled routes  */}
-                <Router path='saved' element={<Saved />} />
-                {/* <Router path='wallets' element={<Wallets />} /> */}
-                <Router path='create' element={<Create />} />
-                <Router path='change-password' element={<ChangePassword />} />
-                <Router path='account' element={<Account />} />
-                <Router path='api-keys' element={<ApiKeys />} />
-                <Router path='settings' element={<Settings />} />
-                {/* <Router path='logs' element={<Logs />} /> */}
-                {/* <Router path='docs' element={<Doc />} /> */}
-                <Router path='about' element={<About />} />
-                <Router path='logs' element={<Log />} />
-                <Router path='log/:id' element={<Log />} />
-                <Router path='successful/:id' element={<Log />} />
-                <Router path={'failed/:id'} element={<Log />} />
-                <Router path='webhook' element={<Webhook />} />
+          <Routes>
+            {user?.role === 'admin' ? (
+              <Router element={<AdminRoute />}>
+                <Router path='/' element={<ManageUsers />} />
+                <Router path='/admin/users/create' element={<CreateUser />} />
+                <Router path='/admin/user/edit/:id' element={<EditUser />} />
+                <Router path='/admin/user/:id' element={<ViewUser />} />
+                <Router path='/admin/user/edit/update-role/:id' element={<UpdateRole />} />
               </Router>
-              ) : (
+            ) : (
               <Router>
                 <Router element={<PrivateRoute />}>
                   <Router path='/' element={<Home />} />
@@ -132,7 +107,7 @@ const Route = () => {
                   <Router path='api-keys' element={<ApiKeys />} />
                   <Router path='settings' element={<Settings />} />
                   {/* <Router path='logs' element={<Logs />} /> */}
-                  <Router path='doc' element={<Doc />} />
+                  {/* <Router path='docs' element={<Doc />} /> */}
                   <Router path='about' element={<About />} />
                   <Router path='logs' element={<Log />} />
                   <Router path='log/:id' element={<Log />} />
@@ -173,36 +148,14 @@ const Route = () => {
                   <Router path={'log/:id'} element={<Log />} />
                   <Router path={'successful/:id'} element={<Log />} />
                   <Router path={'failed/:id'} element={<Log />} />
+                  {/* <Router path={'docs'} element={<Doc />} /> */}
                 </Router>
 
                 <Router path='*' element={<MainComponent value={'page not found'} />} />
               </Router>
-              <Router path={'collection/:collectionId'} element={<CollectionRoute />}>
-                <Router path={'general'} element={<EditCollection />} />
-                <Router path={'assets'} element={<Assets />} />
-                <Router path={'assets/import'} element={<ImportAssets />} />
-                <Router path={'assets/import-images'} element={<ImportImages />} />
-                {/* <Router path={'assets/:assetId'} element={<EditAsset />} /> */}
-                <Router path={'properties'} element={<Properties />} />
-                {/* <Router path={'properties/:propertyId'} element={<EditProperty />} /> */}
-              </Router>
-              <Router path={'player/:playerId'} element={<PlayerRoute />}>
-                <Router path={'general'} element={<PlayerInfo />} />
-                <Router path={'assets'} element={<PlayerAssets />} />
-                <Router path={'transactions'} element={<PlayerTransactions />} />
-              </Router>
-              <Router path={'developers'} element={<DevelopersRoute />}>
-                <Router path={'api-keys'} element={<ApiKeys />} />
-                <Router path={'webhook'} element={<Webhook />} />
-                <Router path={'logs'} element={<Log />} />
-                <Router path={'log/:id'} element={<Log />} />
-                <Router path={'successful/:id'} element={<Log />} />
-                <Router path={'failed/:id'} element={<Log />} />
-                {/* <Router path={'docs'} element={<Doc />} /> */}
-              </Router>
-              <Router path='*' element={<MainComponent value={'page not found'} />} />
-            </Router>
-          )}
+            )}
+          </Routes>
+          <Spotlight />
         </>
       )}
       <Routes>
@@ -217,6 +170,7 @@ const Route = () => {
           <Router path='/cheat-code' element={<CheatCode />} />
         </Router>
       </Routes>
+      <DeleteConfirmationModal />
       <DeleteConfirmationModal />
       <SpotlightModal />
       <ContactInfoModal />
