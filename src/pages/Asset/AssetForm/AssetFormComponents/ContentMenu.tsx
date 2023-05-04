9a8a9a11162fa5ccc87bfe19c476a3478cf151c0
levanion <levanion1998@gmail.com>
2023-05-04 19:02:58 +0400

    fix: asset form update

--- src/pages/Asset/AssetForm/AssetFormComponents/ContentMenu.tsx
+++ src/pages/Asset/AssetForm/AssetFormComponents/ContentMenu.tsx
@@ -7,6 +7,8 @@ import Avatar from '@l3-lib/ui-core/dist/Avatar'
 
 import Close from '@l3-lib/ui-core/dist/icons/Close'
 import SearchOutline from '@l3-lib/ui-core/dist/icons/SearchOutline'
+import MenuListItem from './MenuListItem'
+import { useEffect, useState } from 'react'
 
 type ContentMenuProps = {
   title: string
@@ -16,69 +18,98 @@ type ContentMenuProps = {
 }
 const ContentMenu = ({ title, onClose, items, formik }: ContentMenuProps) => {
   const { asset_properties } = formik?.values
+
+  const [newValues, setNewValues] = useState(asset_properties)
+  const [showApplyButton, setShowApplyButton] = useState(false)
+
+  useEffect(() => {
+    setNewValues(asset_properties)
+  }, [asset_properties])
+
   return (
-    <StyledMenu>
-      <StyledHeader>
-        <Typography
-          value={title}
-          type={Typography.types.LABEL}
-          size={Typography.sizes.md}
-          customColor={'#FFF'}
-        />
+    <>
+      <StyledMenu showApplyButton={showApplyButton}>
+        <StyledHeader>
+          <Typography
+            value={title}
+            type={Typography.types.LABEL}
+            size={Typography.sizes.md}
+            customColor={'#FFF'}
+          />
 
-        <IconButton
-          size={IconButton.sizes.SMALL}
-          icon={Close}
-          kind={IconButton.kinds.TERTIARY}
-          onClick={onClose}
-        />
-      </StyledHeader>
+          <IconButton
+            size={IconButton.sizes.SMALL}
+            icon={Close}
+            kind={IconButton.kinds.TERTIARY}
+            onClick={() => {
+              setShowApplyButton(false)
+              setNewValues(asset_properties)
+              onClose()
+            }}
+          />
+        </StyledHeader>
 
-      <TextField placeholder='Search' iconName={SearchOutline} />
+        <TextField placeholder='Search' iconName={SearchOutline} />
 
-      <StyledSearchedItems>
-        {items?.map((item: any) => {
-          return (
-            <StyledSearchItem
-              selected={asset_properties.includes(item.id)}
-              key={item.id}
-              onClick={() => {
-                if (asset_properties.includes(item.id)) {
-                  const newValues = asset_properties.replace(`${item.id}`, '')
-                  formik.setFieldValue('asset_properties', newValues)
-                } else if (asset_properties.length > 0) {
-                  formik.setFieldValue('asset_properties', `${asset_properties}, ${item.id}`)
-                } else {
-                  formik.setFieldValue('asset_properties', `${item.id}`)
-                }
-              }}
-            >
-              <Avatar
-                size={Avatar.sizes.SMALL}
-                src={item.main_media}
-                type={Avatar.types.IMG}
-                rectangle
+        <StyledSearchedItems>
+          {items?.map((item: any) => {
+            return (
+              <MenuListItem
+                secondary
+                key={item.id}
+                name={item.name}
+                image={item.main_media}
+                selected={newValues.includes(item.id)}
+                onClick={() => {
+                  // if (asset_properties.includes(item.id)) {
+                  //   const newsValues = asset_properties.replace(`${item.id}`, '')
+                  //   formik.setFieldValue('asset_properties', newsValues)
+                  // } else if (asset_properties.length > 0) {
+                  //   formik.setFieldValue('asset_properties', `${asset_properties}, ${item.id}`)
+                  // } else {
+                  //   formik.setFieldValue('asset_properties', `${item.id}`)
+                  // }
+                  if (newValues.includes(item.id)) {
+                    setNewValues(newValues.replace(`${item.id}`, ''))
+                  } else {
+                    setNewValues(`${newValues},${item.id}`)
+                  }
+                  setShowApplyButton(true)
+                }}
               />
-              {item.name}
-            </StyledSearchItem>
-          )
-        })}
-      </StyledSearchedItems>
-    </StyledMenu>
+            )
+          })}
+        </StyledSearchedItems>
+      </StyledMenu>
+      <StyledFooter showApplyButton={showApplyButton}>
+        <StyledFooterButton
+          onClick={async () => {
+            await formik.setFieldValue('asset_properties', newValues)
+            // setNewValues(asset_properties)
+            onClose()
+            setShowApplyButton(false)
+          }}
+        >
+          <Typography
+            value={'Apply'}
+            type={Typography.types.LABEL}
+            size={Typography.sizes.md}
+            customColor={'#FFF'}
+          />
+        </StyledFooterButton>
+      </StyledFooter>
+    </>
   )
 }
 
 export default ContentMenu
 
-const StyledMenu = styled.div`
+const StyledMenu = styled.div<{ showApplyButton: boolean }>`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
   padding: 16px 12px 8px;
   gap: 4px;
-
-  width: 244px;
-  height: 289px;
 
   background: rgba(0, 0, 0, 0.7);
   /* Drop shadow + blur/popover */
@@ -87,7 +118,10 @@ const StyledMenu = styled.div`
   backdrop-filter: blur(50px);
   /* Note: backdrop-filter has minimal browser support */
 
-  border-radius: 6px;
+  border-radius: 6px 6px 0px 0px;
+
+  width: 244px;
+  height: 318px;
 `
 export const StyledHeader = styled.div`
   width: 100%;
@@ -99,6 +133,7 @@ export const StyledHeader = styled.div`
 `
 const StyledSearchedItems = styled.div`
   width: 100%;
+
   margin-top: 12px;
   padding-top: 10px;
   border-top: 1px solid rgba(255, 255, 255, 0.1);
@@ -137,4 +172,41 @@ const StyledSearchItem = styled.div<{ selected: boolean }>`
         background-color: rgba(255, 255, 255, 0.5);
       }
     `};
+`
+const StyledFooter = styled.div<{ showApplyButton: boolean }>`
+  width: 100%;
+
+  padding: 8px;
+  padding-top: 0px;
+
+  display: flex;
+
+  background: rgba(0, 0, 0, 0.7);
+
+  box-shadow: 2px 6px 15px rgba(0, 0, 0, 0.25);
+  backdrop-filter: blur(50px);
+
+  border-radius: 0px 0px 6px 6px;
+
+  opacity: 0;
+
+  max-height: 0px;
+  transition: opacity 0.3s, max-height 0.3s;
+  ${p =>
+    p.showApplyButton &&
+    css`
+      opacity: 1;
+      max-height: 100px;
+    `};
+`
+const StyledFooterButton = styled.div`
+  border-top: 1px solid rgba(255, 255, 255, 0.1);
+  width: 100%;
+  cursor: pointer;
+  height: fit-content;
+
+  padding: 8px;
+  padding-top: 12px;
+  display: flex;
+  justify-content: flex-end;
 `
