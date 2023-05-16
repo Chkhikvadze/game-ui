ad0882e9616f2f05145e3efd8c9854976f009f9c
jemiks <jelo.kokhodze@gmail.com>
2023-05-16 17:16:51 +0400

    fix: collection cards

--- src/modals/AIChatModal/components/GameMedias.tsx
+++ src/modals/AIChatModal/components/GameMedias.tsx
@@ -1,15 +1,32 @@
 import styled from 'styled-components'
 import ImageCard from './ImageCard'
 import { WrapperSecondary } from './WrapperSecondary'
+import { useState } from 'react'
 
 const GameMedias = ({ message }: any) => {
-  console.log(message, 'message')
+  const [items, setItems] = useState<any>([])
 
+  const handleClick = (item: any) => {
+    if (items.includes(item)) {
+      // Remove the item from the array
+      const updatedItems = items.filter((i: any) => i !== item)
+      setItems(updatedItems)
+    } else {
+      // Push the item into the array
+      const updatedItems = items.concat(item)
+      setItems(updatedItems)
+    }
+  }
   return (
     <WrapperSecondary>
       <StyledImageWrapper>
         {message?.medias?.map((item: any) => (
-          <ImageCard key={item} src={item} />
+          <ImageCard
+            key={item}
+            src={item}
+            isSelected={items.includes(item)}
+            onClick={() => handleClick(item)}
+          />
         ))}
       </StyledImageWrapper>
     </WrapperSecondary>
--- src/modals/AIChatModal/components/ImageCard.tsx
+++ src/modals/AIChatModal/components/ImageCard.tsx
@@ -1,24 +1,67 @@
 import styled from 'styled-components'
+import MarkedIconOutlineSvg from '../assets/MarkedIconOutlineSvg'
 
-const ImageCard = ({ src }: any) => {
+const ImageCard = ({ src, isSelected, ...props }: any) => {
   return (
-    <StyledImageContainer>
+    <StyledImageContainer isSelected={isSelected} {...props}>
       <img src={src} alt='media' />
+      {isSelected && (
+        <StyledSvgContainer>
+          <MarkedIconOutlineSvg />
+        </StyledSvgContainer>
+      )}
     </StyledImageContainer>
   )
 }
 
 export default ImageCard
 
-const StyledImageContainer = styled.div`
+const StyledImageContainer = styled.div<{ isSelected?: boolean }>`
   position: relative;
   width: 100%;
   height: 100%;
   object-fit: contain;
+  box-shadow: 0px 3.52941px 10.5882px rgba(0, 0, 0, 0.15);
+  backdrop-filter: blur(176.471px);
+  border-radius: 10px;
+  overflow: hidden;
   img {
     position: absolute;
     width: 100%;
     height: 100%;
     left: 0;
   }
+
+  ${({ isSelected }) =>
+    !isSelected &&
+    `
+    border: 2px solid rgba(255, 255, 255, 0.3);
+  
+  `}
+
+  ${({ isSelected }) =>
+    isSelected &&
+    `
+  
+  &::after {
+    content: '';
+    position: absolute;
+    inset: 0;
+    border-radius: 10px;
+    padding: 1.5px;
+    background: linear-gradient(180deg, #73fafd 0%, #50b1d7 100%);
+    -webkit-mask: linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0);
+    -webkit-mask-composite: xor;
+    mask-composite: exclude;
+    pointer-events: none;
+    border: none;
+  }
+}
+  `}
+`
+
+const StyledSvgContainer = styled.div`
+  position: absolute;
+  right: 7px;
+  top: 8px;
 `
