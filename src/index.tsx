230e6f92b8cf449dfe92353dabe67ee91dda5b75
Mirian Okradze <okradzemirian@gmail.com>
2023-06-09 12:15:05 +0400

    feat: disable sentry on local environment

--- src/index.tsx
+++ src/index.tsx
@@ -16,7 +16,7 @@ Sentry.init({
   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
   environment: process.env.REACT_APP_ENV,
-  // enabled: process.env.REACT_APP_ENV !== 'local',
+  enabled: process.env.REACT_APP_ENV !== 'local',
 })
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
