acf7449370eff7bd013a04a1771d857bd60fad13
DavitBakhutashvili <davit.bakhutashvili1@gmail.com>
2023-06-09 15:35:35 +0400

    fix: updated issues

--- src/pages/Collection/Collections/Collections.tsx
+++ src/pages/Collection/Collections/Collections.tsx
@@ -65,6 +65,9 @@ const default_image = Action
 import { useContractByCollectionIdService } from 'services'
 import ContractChain from 'components/ContractChains/ContractChain'
 
+// const default_image =
+//   'https://i.guim.co.uk/img/media/01512e0bd1d78a9a85026844386c02c544c01084/38_0_1200_720/master/1200.jpg?width=1200&quality=85&auto=format&fit=max&s=cef05f7f90efd180648f5aa5ce0d3690'
+
 const default_logo =
   'https://upload.wikimedia.org/wikipedia/commons/7/7c/Fortnite_F_lettermark_logo.png'
 
@@ -83,217 +86,209 @@ const Collections = () => {
 
   const [activeTab, setActiveTab] = useState(0)
 
-  const renderCollectionCard = (item: any) => {
-    const { main_media, medias, categories } = item
-    console.log('items', categories)
-    const CollectionCard = (item: any) => {
-      const { main_media, medias, id: collectionId } = item
+  const CollectionCard = (item: any) => {
+    const { main_media, medias, id: collectionId } = item
 
-      const media_video = findVideo(medias)
+    const media_video = findVideo(medias)
 
-      const item_info = {
-        title: item.name,
-        description: item.description,
-        subTitle: '101 Owners',
-        logo: item.logo_image,
-        image: item.cover_image,
-        created: item.created_on,
-      }
-
-      const categoryValues = item.categories.map((category: any) => category.value)
-
-      const categoryToDefaultImageMap: { [key: string]: string } = {
-        Shields: Shields,
-        Helmets: Helmets,
-        // Sea:,
-        // Jungle:,
-        // Birds:,
-        // Reptile:,
-        // Mammals:,
-        Jewelry: Jewellery,
-        // Tattos:,
-        Glasses: Glasses,
-        // Coins:,
-        Cards: Cards,
-        // Cars:,
-        // Villages:,
-        Worlds: Worlds,
-        // Gems:,
-        // Boards:,
-        // 'Front wing':,
-        Wheels: SteerinfWheels,
-        Tyres: Tyres,
-        Motors: Motors,
-        Nitros: Nitros,
-        //  'Bulletproof vest':,
-        Skins: Skins,
-        Backpacks: Backpacks,
-        Weapons: Wepons,
-        'Easter eggs': Eastereggs,
-        Poisons: Poison,
-        Reptile: Reptitels,
-        Vehicles: Vehicles,
-        Houses: Houses,
-        Animals: Animals,
-        Ships: Ships,
-        Clothes: Clothes,
-        Accessories: Accessories,
-        Trophies: Trophys,
-        // Balls:,
-        Castles: Castles,
-        Lands: Lands,
-        Warriors: Warriors,
-        // 'Military vehicles':,
-        Goods: Gods,
-      }
-      const default_collection_image =
-        main_media || categoryToDefaultImageMap[categoryValues[0]] || default_image
-      const { data: collectionContract } = useContractByCollectionIdService({
-        id: collectionId,
-      })
-
-      const price = collectionContract?.config?.player_mint_fee
-      const contractChain = collectionContract?.blockchain
-
-      return (
-        <GameCard
-          key={item.id}
-          size={'medium'}
-          onImageClick={() => navigate(`/collection/${item.id}/general`)}
-          // onButtonClick={async () => {
-          //   handleCardClick(item.id)
-          //   await refetchCollection()
-          //   refetchPlayers
-          // }}
-          itemInfo={item_info}
-          defaultLogo={default_logo}
-          defaultImage={default_collection_image}
-          details={
-            <CollectionDetail
-              price={{ minPrice: price || 0, volume: 123000, listed: 3 }}
-              owners={{ ownerImages: OWNER_IMAGES, ownerCount: 101 }}
-              assets={{ assetImages: ASSET_IMAGES, assetCount: 101 }}
-            />
-          }
-          cardFooter={<CollectionFooter title={item.name} subTitle={'101 Owners'} />}
-          topLeftIcon={contractChain && <ContractChain chainName={contractChain} />}
-          topRightIcon={
-            price && (
-              <StyledTopRightIcon>
-                <Typography
-                  value={price}
-                  type={Typography.types.LABEL}
-                  size={Typography.sizes.LARGE}
-                />
-              </StyledTopRightIcon>
-            )
-          }
-          // minPrice={0.96}
-          video={media_video ? media_video['url'] : ''}
-        />
-      )
+    const item_info = {
+      title: item.name,
+      description: item.description,
+      subTitle: '101 Owners',
+      logo: item.logo_image,
+      image: item.cover_image,
+      created: item.created_on,
     }
 
-    const allCollections = data?.items
-    const activeCollections = data?.items?.filter((item: any) => item.status === 'Active')
-    const draftCollections = data?.items?.filter((item: any) => item.status === 'Draft')
+    const categoryValues = item.categories.map((category: any) => category.value)
 
-    const allCollectionsCount = allCollections?.length
-    const activeCollectionsCount = activeCollections?.length
-    const draftCollectionsCount = draftCollections?.length
+    const categoryToDefaultImageMap: { [key: string]: string } = {
+      Shields: Shields,
+      Helmets: Helmets,
+      // Sea:,
+      // Jungle:,
+      // Birds:,
+      // Reptile:,
+      // Mammals:,
+      Jewelry: Jewellery,
+      // Tattos:,
+      Glasses: Glasses,
+      // Coins:,
+      Cards: Cards,
+      // Cars:,
+      // Villages:,
+      Worlds: Worlds,
+      // Gems:,
+      // Boards:,
+      // 'Front wing':,
+      Wheels: SteerinfWheels,
+      Tyres: Tyres,
+      Motors: Motors,
+      Nitros: Nitros,
+      //  'Bulletproof vest':,
+      Skins: Skins,
+      Backpacks: Backpacks,
+      Weapons: Wepons,
+      'Easter eggs': Eastereggs,
+      Poisons: Poison,
+      Reptile: Reptitels,
+      Vehicles: Vehicles,
+      Houses: Houses,
+      Animals: Animals,
+      Ships: Ships,
+      Clothes: Clothes,
+      Accessories: Accessories,
+      Trophies: Trophys,
+      // Balls:,
+      Castles: Castles,
+      Lands: Lands,
+      Warriors: Warriors,
+      // 'Military vehicles':,
+      Goods: Gods,
+    }
+    const default_collection_image =
+      main_media || categoryToDefaultImageMap[categoryValues[0]] || default_image
+    const { data: collectionContract } = useContractByCollectionIdService({
+      id: collectionId,
+    })
+
+    const price = collectionContract?.config?.player_mint_fee
+    const contractChain = collectionContract?.blockchain
 
     return (
-      <>
-        <HeaderWrapper>
-          <StyledHeaderGroup>
-            <TabList>
-              <Tab onClick={() => setActiveTab(0)}>All</Tab>
-              <Tab onClick={() => setActiveTab(1)}>Active</Tab>
-              <Tab onClick={() => setActiveTab(2)}>Draft</Tab>
-            </TabList>
-            <Button size={Button.sizes.MEDIUM} onClick={onCreateCollection} leftIcon={Add}>
+      <GameCard
+        key={item.id}
+        size={'medium'}
+        onImageClick={() => navigate(`/collection/${item.id}/general`)}
+        // onButtonClick={async () => {
+        //   handleCardClick(item.id)
+        //   await refetchCollection()
+        //   refetchPlayers
+        // }}
+        itemInfo={item_info}
+        defaultLogo={default_logo}
+        defaultImage={default_collection_image}
+        details={
+          <CollectionDetail
+            price={{ minPrice: price || 0, volume: 123000, listed: 3 }}
+            owners={{ ownerImages: OWNER_IMAGES, ownerCount: 101 }}
+            assets={{ assetImages: ASSET_IMAGES, assetCount: 101 }}
+          />
+        }
+        cardFooter={<CollectionFooter title={item.name} subTitle={'101 Owners'} />}
+        topLeftIcon={contractChain && <ContractChain chainName={contractChain} />}
+        topRightIcon={
+          price && (
+            <StyledTopRightIcon>
               <Typography
-                value={'Create'}
+                value={price}
                 type={Typography.types.LABEL}
-                size={Typography.sizes.md}
+                size={Typography.sizes.LARGE}
               />
-            </Button>
-          </StyledHeaderGroup>
-        </HeaderWrapper>
-
-        <StyledInnerWrapper>
-          <TabsContext activeTabId={activeTab} className='tab_pannels_container'>
-            <TabPanels>
-              <TabPanel>
-                {activeCollectionsCount > 0 && (
-                  <>
-                    <TabHeader heading='Active' paragraph='Game which are successfully deployed' />
-                    <StyledContainerWrapper className='wrapper_card'>
-                      {activeCollections?.slice(0, 4).map((item: any) => {
-                        return <CollectionCard {...item} key={item.id} />
-                      })}
-                      {activeCollectionsCount > 4 && (
-                        <Button onClick={() => setActiveTab(1)} kind='tertiary'>
-                          See all
-                        </Button>
-                      )}
-                    </StyledContainerWrapper>
-                  </>
-                )}
-
-                {draftCollectionsCount > 0 && (
-                  <>
-                    <TabHeader heading='Draft' paragraph='Game which are successfully deployed' />
-                    <StyledContainerWrapper className='wrapper_card'>
-                      {draftCollections?.slice(0, 4).map((item: any) => {
-                        return <CollectionCard {...item} key={item.id} />
-                      })}
-                      {draftCollectionsCount > 4 && (
-                        <Button onClick={() => setActiveTab(2)} kind='tertiary'>
-                          See all
-                        </Button>
-                      )}
-                    </StyledContainerWrapper>
-                  </>
-                )}
-                {allCollectionsCount === 0 && <CollectionPagesEmptyScreen />}
-              </TabPanel>
-
-              <TabPanel>
-                {activeCollectionsCount > 0 && (
-                  <>
-                    <TabHeader heading='Active' paragraph='Game which are successfully deployed' />
-                    <StyledContainerWrapper className='wrapper_card'>
-                      {activeCollections?.map((item: any) => {
-                        return <CollectionCard {...item} key={item.id} />
-                      })}
-                    </StyledContainerWrapper>
-                  </>
-                )}
-                {activeCollectionsCount === 0 && <CollectionPagesEmptyScreen />}
-              </TabPanel>
-
-              <TabPanel>
-                {draftCollectionsCount > 0 && (
-                  <>
-                    <TabHeader heading='Draft' paragraph='Game which are successfully deployed' />
-                    <StyledContainerWrapper className='wrapper_card'>
-                      {draftCollections?.map((item: any) => {
-                        return <CollectionCard {...item} key={item.id} />
-                      })}
-                    </StyledContainerWrapper>
-                  </>
-                )}
-
-                {draftCollectionsCount === 0 && <CollectionPagesEmptyScreen />}
-              </TabPanel>
-            </TabPanels>
-          </TabsContext>
-        </StyledInnerWrapper>
-        {/* <CreateCollectionModal /> */}
-      </>
+            </StyledTopRightIcon>
+          )
+        }
+        // minPrice={0.96}
+        video={media_video ? media_video['url'] : ''}
+      />
     )
   }
+
+  const allCollections = data?.items
+  const activeCollections = data?.items?.filter((item: any) => item.status === 'Active')
+  const draftCollections = data?.items?.filter((item: any) => item.status === 'Draft')
+
+  const allCollectionsCount = allCollections?.length
+  const activeCollectionsCount = activeCollections?.length
+  const draftCollectionsCount = draftCollections?.length
+
+  return (
+    <>
+      <HeaderWrapper>
+        <StyledHeaderGroup>
+          <TabList>
+            <Tab onClick={() => setActiveTab(0)}>All</Tab>
+            <Tab onClick={() => setActiveTab(1)}>Active</Tab>
+            <Tab onClick={() => setActiveTab(2)}>Draft</Tab>
+          </TabList>
+          <Button size={Button.sizes.MEDIUM} onClick={onCreateCollection} leftIcon={Add}>
+            <Typography value={'Create'} type={Typography.types.LABEL} size={Typography.sizes.md} />
+          </Button>
+        </StyledHeaderGroup>
+      </HeaderWrapper>
+
+      <StyledInnerWrapper>
+        <TabsContext activeTabId={activeTab} className='tab_pannels_container'>
+          <TabPanels>
+            <TabPanel>
+              {activeCollectionsCount > 0 && (
+                <>
+                  <TabHeader heading='Active' paragraph='Game which are successfully deployed' />
+                  <StyledContainerWrapper className='wrapper_card'>
+                    {activeCollections?.slice(0, 4).map((item: any) => {
+                      return <CollectionCard {...item} key={item.id} />
+                    })}
+                    {activeCollectionsCount > 4 && (
+                      <Button onClick={() => setActiveTab(1)} kind='tertiary'>
+                        See all
+                      </Button>
+                    )}
+                  </StyledContainerWrapper>
+                </>
+              )}
+
+              {draftCollectionsCount > 0 && (
+                <>
+                  <TabHeader heading='Draft' paragraph='Game which are successfully deployed' />
+                  <StyledContainerWrapper className='wrapper_card'>
+                    {draftCollections?.slice(0, 4).map((item: any) => {
+                      return <CollectionCard {...item} key={item.id} />
+                    })}
+                    {draftCollectionsCount > 4 && (
+                      <Button onClick={() => setActiveTab(2)} kind='tertiary'>
+                        See all
+                      </Button>
+                    )}
+                  </StyledContainerWrapper>
+                </>
+              )}
+              {allCollectionsCount === 0 && <CollectionPagesEmptyScreen />}
+            </TabPanel>
+
+            <TabPanel>
+              {activeCollectionsCount > 0 && (
+                <>
+                  <TabHeader heading='Active' paragraph='Game which are successfully deployed' />
+                  <StyledContainerWrapper className='wrapper_card'>
+                    {activeCollections?.map((item: any) => {
+                      return <CollectionCard {...item} key={item.id} />
+                    })}
+                  </StyledContainerWrapper>
+                </>
+              )}
+              {activeCollectionsCount === 0 && <CollectionPagesEmptyScreen />}
+            </TabPanel>
+
+            <TabPanel>
+              {draftCollectionsCount > 0 && (
+                <>
+                  <TabHeader heading='Draft' paragraph='Game which are successfully deployed' />
+                  <StyledContainerWrapper className='wrapper_card'>
+                    {draftCollections?.map((item: any) => {
+                      return <CollectionCard {...item} key={item.id} />
+                    })}
+                  </StyledContainerWrapper>
+                </>
+              )}
+
+              {draftCollectionsCount === 0 && <CollectionPagesEmptyScreen />}
+            </TabPanel>
+          </TabPanels>
+        </TabsContext>
+      </StyledInnerWrapper>
+      {/* <CreateCollectionModal /> */}
+    </>
+  )
 }
 
 export default Collections
