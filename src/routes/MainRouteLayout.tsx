c95a9e8c9d72b370a5d0dc370c84cb2f1ae3481a
levanion <levanion1998@gmail.com>
2023-07-05 10:45:17 +0400

    feat: chat switcher

--- src/routes/MainRouteLayout.tsx
+++ src/routes/MainRouteLayout.tsx
@@ -16,6 +16,8 @@ import {
 import AvatarDropDown from 'components/AvatarDropDown'
 
 import Spotlight from 'components/Spotlight/Spotlight'
+import styled from 'styled-components'
+import ChatSwitcher from 'components/ChatSwitcher'
 
 const MainRouteLayout = () => {
   const { user } = React.useContext(AuthContext)
@@ -46,8 +48,31 @@ const MainRouteLayout = () => {
         <div></div>
       </StyledFooter>
       {/* </StyledMainLayout> */}
+      {/* <StyledChatSwitcher></StyledChatSwitcher> */}
+      <ChatSwitcher />
     </StyledAppContainer>
   )
 }
 
 export default MainRouteLayout
+
+// const StyledChatSwitcher = styled.div`
+//   display: inline-flex;
+//   padding: 10px;
+//   flex-direction: column;
+//   justify-content: center;
+//   align-items: center;
+//   gap: 10px;
+
+//   border-radius: 100px;
+//   background: rgba(255, 255, 255, 0.1);
+//   /* Style */
+//   box-shadow: 0px 8px 6px 0px rgba(0, 0, 0, 0.05), 0px 1px 1px 0px rgba(255, 255, 255, 0.25) inset,
+//     0px -1px 1px 0px rgba(255, 255, 255, 0.1) inset;
+//   backdrop-filter: blur(50px);
+
+//   position absolute;
+//   top: 50%;
+//   left: 32px;
+
+// `
