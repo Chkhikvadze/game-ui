187ee1de35dda263522b84e9457fe79712fcbaa9
jemiks <jelo.kokhodze@gmail.com>
2023-07-20 09:50:47 +0400

    fix: update user

--- src/assets/avatars/index.ts
+++ src/assets/avatars/index.ts
@@ -3,10 +3,15 @@ import PathTwo from 'assets/avatars/Path_2.svg'
 import PathThree from 'assets/avatars/Path_3.svg'
 import PathFour from 'assets/avatars/Path_4.svg'
 import Avatar_1 from 'assets/avatars/azuki_2.jpg'
-import Avatar_2 from 'assets/avatars/monkey_2.jpg'
-import Avatar_3 from 'assets/avatars/monkey_3.jpg'
+import Avatar_2 from 'assets/avatars/azuki_3.jpg'
+import Avatar_3 from 'assets/avatars/azuki_4.jpg'
+import Avatar_4 from 'assets/avatars/monkey_1.jpg'
+import Avatar_5 from 'assets/avatars/monkey_2.jpg'
+import Avatar_6 from 'assets/avatars/monkey_3.jpg'
 import DefaultIcon from 'assets/icons/default_icon.svg'
 import TwitterIcon from 'assets/icons/twitter_icon.svg'
+
+const avatarsArray = [Avatar_1, Avatar_2, Avatar_3, Avatar_4, Avatar_5, Avatar_6]
 
 export {
   PathOne,
@@ -18,4 +23,5 @@ export {
   Avatar_3,
   DefaultIcon,
   TwitterIcon,
+  avatarsArray,
 }
